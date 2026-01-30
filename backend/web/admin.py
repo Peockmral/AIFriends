379552c8aca793a47a2da388ff15a5bfc02904a0
Peockmral <w19861707427@126.com>
2026-01-30 22:44:47 +0800

    后端实现登录和注册

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from web.models.user import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)           #逗号不要删，否则就不是列表
+
 
 # Register your models here.
