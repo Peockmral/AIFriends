06239067ebb1858b2586b07847e449b5d30909af
Peockmral <w19861707427@126.com>
2026-03-05 23:17:56 +0800

    添加系统提示词；实现短期记忆

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from web.models.user import UserProfile
 from web.models.character import Character
-from web.models.friend import Friend
+from web.models.friend import Friend, SystemPrompt
 from web.models.friend import Message
 
 # Register your models here.
@@ -22,3 +22,4 @@ class FriendAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     raw_id_fields = ('friend',)
 
+admin.site.register(SystemPrompt)
